8ed1eb44ee8d0a630915d435454423399b7904e8
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-05-04 12:02:05 +0200

    added root functionality and root checker

--- app/src/main/java/de/visi0nary/app2system/AppFragment.java
+++ app/src/main/java/de/visi0nary/app2system/AppFragment.java
@@ -3,6 +3,7 @@ package de.visi0nary.app2system;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -12,7 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,8 +41,65 @@ public class AppFragment extends ListFragment {
 
 
     private void moveApp(ApplicationInfo appInfo) {
+        if (deviceIsRooted()) {
+            Process suProcess = null;
+            try {
+                // start an SU process
+                suProcess = Runtime.getRuntime().exec("su");
+                DataOutputStream os = new DataOutputStream(suProcess.getOutputStream());
 
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if(suProcess!=null)
+                    // clean up
+                    suProcess.destroy();
+            }
+        }
+        //we're not rooted
+        else {
+            Toast.makeText(getActivity().getApplicationContext(), "Couldn't acquire root. :(", Toast.LENGTH_LONG).show();
+        }
     }
+
+    // returns true if device is rooted and false if not
+    private boolean deviceIsRooted() {
+        int returnValue = -1;
+        // create temporary SU process
+        Process testSU = null;
+        try {
+            // try to create a root process
+            testSU = Runtime.getRuntime().exec("su");
+            DataOutputStream testingStream = new DataOutputStream(testSU.getOutputStream());
+            // exit the process again
+            testingStream.writeBytes("exit\n");
+            testingStream.flush();
+            try {
+                // wait for the SU process to terminate (if it exists)
+                testSU.waitFor();
+                // check for the exit value (0 if it was a root process, 255 if not)
+                returnValue = testSU.exitValue();
+            } catch (Exception e) {
+                Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally {
+            // clean up
+            if(testSU!=null) {
+                testSU.destroy();
+            }
+        }
+        return(returnValue==0);
+    }
+
+
+
 
     // inner factory class
     public class MoveAlertDialogFactory {
@@ -79,7 +140,7 @@ public class AppFragment extends ListFragment {
                         public void onClick(DialogInterface dialogInterface, int i) {
                             //user clicked ok
                             final Long temp = new Long(appId);
-                            moveApp(systemAppList.get(temp.intValue()));
+                            moveApp(userAppList.get(temp.intValue()));
                         }
                     });
 
