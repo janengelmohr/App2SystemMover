6e4c60314b2e2a44c323ce62abe5eeae83190537
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-05-10 14:51:39 +0200

    added view class for list items added model class for list items

---
+++ app/src/main/java/de/visi0nary/app2system/Views/AppListView.java
@@ -0,0 +1,30 @@
+package de.visi0nary.app2system.Views;
+
+/**
+ * Created by visi0nary on 10.05.15.
+ */
+public class AppListView {
+
+    private ImageView appIcon;
+    private TextView appName;
+
+
+    public TextView getAppName() {
+        return appName;
+    }
+
+    public void setAppName(TextView appName) {
+        this.appName = appName;
+    }
+
+    public ImageView getAppIcon() {
+        return appIcon;
+    }
+
+    public void setAppIcon(ImageView appIcon) {
+        this.appIcon = appIcon;
+    }
+
+
+
+}
