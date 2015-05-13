8933435192bdcc46378eef3d3855d6c84e11fcf9
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-05-13 12:26:12 +0200

    removed unused imports

--- app/src/main/java/de/visi0nary/app2system/Fragments/AppFragment.java
+++ app/src/main/java/de/visi0nary/app2system/Fragments/AppFragment.java
@@ -3,25 +3,18 @@ package de.visi0nary.app2system.Fragments;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import de.visi0nary.app2system.R;
 
