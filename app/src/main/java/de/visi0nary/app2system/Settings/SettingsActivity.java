5eeef642797a5fe5916f985795a02fc779e0c3cf
visi0nary <jan.engelmohr@mailbox.tu-dresden.de>
2015-05-31 14:55:59 +0200

    removed unused methods

--- app/src/main/java/de/visi0nary/app2system/Settings/SettingsActivity.java
+++ app/src/main/java/de/visi0nary/app2system/Settings/SettingsActivity.java
@@ -10,17 +10,12 @@ import android.view.MenuItem;
 import de.visi0nary.app2system.R;
 
 
-public class SettingsActivity extends Activity implements AdvancedSettingsFragment.OnFragmentInteractionListener {
+public class SettingsActivity extends Activity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
-
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
+    //no need to implement logic here as this activity is just a wrapper for the advancedsettingsfragment
 }
